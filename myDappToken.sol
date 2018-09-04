1761b73e4df62e9071ac4e713a24a507cc8d985f
SergeiRevenko <23happyami@gmail.com>
2018-09-04 13:26:48 +0300

    new

--- myDappToken.sol
+++ myDappToken.sol
@@ -1,12 +1,20 @@
-import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 pragma solidity ^0.4.24;
 
 
-contract myDappToken is ERC20Mintable {
+contract myDappToken is ERC20,Ownable {
 
   string public name = "SRToken";
   string public symbol = "SRT";
   uint public decimals = 18;
+  
+ uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
 
+ constructor() public {
+    _mint(msg.sender, INITIAL_SUPPLY);
+  }
 }
